1f902e084292e13efef85ffbcf22f4518f35c08d
Lincoln Lorenz <linknum23@gmail.com>
2021-04-01 15:41:49 -0400

    Remove logging to simplify minimal POC

--- server/server.py
+++ server/server.py
@@ -12,12 +12,15 @@ from sh import tail
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 import time
+import datetime
 import os
 import threading
 import logging
+import queue
 
 #create our app instance
 app = FastAPI()
+q = queue.Queue()
 
 real_path = os.path.realpath(__file__)
 dir_path = os.path.dirname(real_path)
@@ -27,11 +30,13 @@ app.mount("/client", StaticFiles(directory=f"{dir_path}/../client"), name="clien
 #This async generator will listen to our log file in an infinite while loop (happens in the tail command)
 #Anytime the generator detects a new line in the log file, it will yield it.
 async def logGenerator(request):
-    for line in tail("-f", LOGFILE, _iter=True):
+    while True:
         if await request.is_disconnected():
             print("client disconnected!!!")
             break
-        yield line
+        if not q.empty():
+            line = q.get()
+            yield line
         time.sleep(0.5)
 
 #This is our api endpoint. When a client subscribes to this endpoint, they will recieve SSE from our log file
@@ -45,20 +50,15 @@ async def get_index():
     return FileResponse('client/client.html')
 
 def worker():
-    logger = logging.getLogger('log_app')
-    logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler(LOGFILE)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
     #infinite while loop printing to our log file.
     i = 0
     while True:
-        logger.info(f"log message num: {i}")
+        d = datetime.datetime.now()
+        q.put(f"{d} - log message num: {i}")
         i += 1
         time.sleep(0.3)
 
 # start the background worker
-t = threading.Thread(worker)
+t = threading.Thread(target=worker)
 t.start()
 t.join()
